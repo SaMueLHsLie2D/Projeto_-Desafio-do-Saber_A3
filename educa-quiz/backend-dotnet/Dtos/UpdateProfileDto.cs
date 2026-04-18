0e1ae973b954bf580755ee1d4433e3937d49aa07
alicebarrosv <alicebarros152152@gmail.com>
2026-04-17 22:45:30 -0300

    Editando UploadController

--- educa-quiz/backend-dotnet/Dtos/UpdateProfileDto.cs
+++ educa-quiz/backend-dotnet/Dtos/UpdateProfileDto.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace backend-dotnet.Dtos
+namespace backend_dotnet.Dtos
 {
     public class UpdateProfileDto
     {
+        public int? AvatarId { get; set; }
+        public string BackgroundColor { get; set; }
         
     }
 }
